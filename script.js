cae2623588b3e21be952d5532526e5c5e8cef80b
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-10-05 16:23:07 -0400

    Finish website

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.addEventListener("click", function(){
-    document.getElementById("email").innerHTML = "hildacarden@gmail.com";
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
+
+const input = document.getElementById('email');
+input.addEventListener('click', function() {
+  alert('You are about to compose an email to send to Hilda Diana');
 });
